87220c70beec6e5428b078a6f70e7566dd8ab2e6
unknown <ronn4157@student.eadania.dk>
2019-11-20 12:27:17 +0100

    added Console writeline

--- ConsoleTest/ConsoleTest/Program.cs
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hellO");
         }
     }
 }
